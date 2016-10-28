8d4bc226501dca061bb7a2054de8b135145a736d
Toshiaki Maki <tmaki@pivotal.io>
2016-10-29 05:31:59 +0900

    Fix wrong javadoc

--- src/main/java/org/springframework/retry/annotation/CircuitBreaker.java
+++ src/main/java/org/springframework/retry/annotation/CircuitBreaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import java.lang.annotation.Target;
  *
  * @author Dave Syer
  * @author Artem Bilan
- * @since 1.1
+ * @since 1.2
  *
  */
 @Target({ ElementType.METHOD, ElementType.TYPE })
