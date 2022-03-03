68ead2af851ef59550a552c162d15b83f98a6a7a
Stephane Nicoll <snicoll@vmware.com>
2022-03-03 08:59:19 +0100

    Upgrade to Maven Surefire Plugin 2.2.22
    This commit also polishes the build as "Tests" is a pattern that is
    recognized automatically. This revealed a number of test classes that
    did not end with Tests and were not executed.

--- src/test/java/org/springframework/classify/BinaryExceptionClassifierBuilderTests.java
+++ src/test/java/org/springframework/classify/BinaryExceptionClassifierBuilderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2006-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import org.springframework.retry.support.RetryTemplate;
 /**
  * @author Aleksandr Shamukov
  */
-public class BinaryExceptionClassifierBuilderTest {
+public class BinaryExceptionClassifierBuilderTests {
 
 	@Test
 	public void testWhiteList() {
--- src/test/java/org/springframework/retry/support/RetryTemplateBuilderTests.java
+++ src/test/java/org/springframework/retry/support/RetryTemplateBuilderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2006-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ import static org.springframework.retry.util.test.TestUtils.getPropertyValue;
  * @author Aleksandr Shamukov
  * @author Kim In Hoi
  */
-public class RetryTemplateBuilderTest {
+public class RetryTemplateBuilderTests {
 
 	/* ---------------- Mixed tests -------------- */
 
