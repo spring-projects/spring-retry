3398f8feab5dbbe158081e9f7380c4611f8b17d4
Gary Russell <grussell@vmware.com>
2022-04-18 14:35:18 -0400

    Switch to Java 17 on CI
    See gh-286

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -2,8 +2,8 @@
 set -e
 
 case "$1" in
-	java8)
-		 echo "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz"
+	java17)
+		 echo "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
