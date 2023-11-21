8f4c603e5e47475ffde341a91de4c73544884926
akenra <37288280+akenra@users.noreply.github.com>
2023-11-21 19:48:11 +0300

    GH-211: Provide the ability to exclude global retryListeners (#392)
    * GH-211: Provide the ability to exclude global retryListeners
    
    Fixes
    https://github.com/spring-projects/spring-retry/issues/211
    
    * Javadoc polishing.
    
    * Javadoc polishing.
    
    ---------
    
    Co-authored-by: akenra <ake.ra@yandex.ru>
    Co-authored-by: Gary Russell <grussell@vmware.com>

--- src/main/java/org/springframework/retry/annotation/Retryable.java
+++ src/main/java/org/springframework/retry/annotation/Retryable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2022 the original author or authors.
+ * Copyright 2014-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ import org.springframework.core.annotation.AliasFor;
  * @author Artem Bilan
  * @author Gary Russell
  * @author Maksim Kita
+ * @author Roman Akentev
  * @since 1.1
  *
  */
@@ -165,7 +166,9 @@ public @interface Retryable {
 
 	/**
 	 * Bean names of retry listeners to use instead of default ones defined in Spring
-	 * context
+	 * context. If this attribute is set to an empty string {@code ""}, it will
+	 * effectively exclude all retry listeners, including with the default listener beans,
+	 * from being used.
 	 * @return retry listeners bean names
 	 */
 	String[] listeners() default {};
