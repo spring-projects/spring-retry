561d41ae257bacfebfc71cbff80b31c8d28fd029
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2023-12-14 15:03:47 +0100

    Upgrade CI to Java 17.0.9

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java17)
-		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz"
+		echo "https://download.bell-sw.com/java/17.0.9+11/bellsoft-jdk17.0.9+11-linux-amd64.tar.gz"
 	;;
 	*)
 		echo $"Unknown java version"
