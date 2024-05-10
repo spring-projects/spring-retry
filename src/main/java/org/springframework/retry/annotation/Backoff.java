70b1e1ba89fff6efc15fafb921ed5210a4044944
Aftab Shaikh <sk.aftab1999@gmail.com>
2024-05-10 20:38:34 +0530

    GH-427: Fix unit tests and documentation

--- src/main/java/org/springframework/retry/annotation/Backoff.java
+++ src/main/java/org/springframework/retry/annotation/Backoff.java
@@ -130,11 +130,9 @@ public @interface Backoff {
 	 * Evaluates to a value. In the exponential case ({@link #multiplier()} &gt; 1.0) set
 	 * this to true to have the backoff delays randomized, so that the maximum delay is
 	 * multiplier times the previous delay and the distribution is uniform between the two
-	 * values. Use {@code #{...}} for one-time evaluation during initialization, omit the
-	 * delimiters for evaluation at runtime. This expression is always evaluated at
-	 * configuration phase. If the expression returns true then
-	 * {@link org.springframework.retry.backoff.ExponentialRandomBackOffPolicy} is used
-	 * else {@link org.springframework.retry.backoff.ExponentialBackOffPolicy} is used.
+	 * values. This expression is always evaluated during initialization. If the expression
+	 * returns true then {@link org.springframework.retry.backoff.ExponentialRandomBackOffPolicy}
+	 * is used else {@link org.springframework.retry.backoff.ExponentialBackOffPolicy} is used.
 	 * @return the flag to signal randomization is required (default false)
 	 */
 	String randomExpression() default "";
--- src/test/java/org/springframework/retry/backoff/BackOffPolicyBuilderTests.java
+++ src/test/java/org/springframework/retry/backoff/BackOffPolicyBuilderTests.java
@@ -120,6 +120,26 @@ public class BackOffPolicyBuilderTests {
 			.randomSupplier(() -> true)
 			.sleeper(mockSleeper)
 			.build();
+
+		assertThat(ExponentialRandomBackOffPolicy.class.isAssignableFrom(backOffPolicy.getClass())).isTrue();
+		ExponentialRandomBackOffPolicy policy = (ExponentialRandomBackOffPolicy) backOffPolicy;
+		assertThat(policy.getInitialInterval()).isEqualTo(10000);
+		assertThat(policy.getMaxInterval()).isEqualTo(100000);
+		assertThat(policy.getMultiplier()).isEqualTo(10);
+		assertThat(new DirectFieldAccessor(policy).getPropertyValue("sleeper")).isEqualTo(mockSleeper);
+	}
+
+	@Test
+	public void shouldCreateExponentialRandomBackOffWithProvidedSuppliers() {
+		Sleeper mockSleeper = mock(Sleeper.class);
+		BackOffPolicy backOffPolicy = BackOffPolicyBuilder.newBuilder()
+				.delaySupplier(() -> 10000L)
+				.maxDelaySupplier(() -> 100000L)
+				.multiplierSupplier(() -> 10d)
+				.randomSupplier(() -> true)
+				.sleeper(mockSleeper)
+				.build();
+
 		assertThat(ExponentialRandomBackOffPolicy.class.isAssignableFrom(backOffPolicy.getClass())).isTrue();
 		ExponentialRandomBackOffPolicy policy = (ExponentialRandomBackOffPolicy) backOffPolicy;
 		assertThat(policy.getInitialInterval()).isEqualTo(10000);
@@ -135,10 +155,10 @@ public class BackOffPolicyBuilderTests {
 			.delay(100)
 			.maxDelay(1000)
 			.multiplier(2)
-			.randomSupplier(() -> true)
+			.randomSupplier(() -> false)
 			.sleeper(mockSleeper)
 			.build();
-		assertThat(ExponentialBackOffPolicy.class.isAssignableFrom(backOffPolicy.getClass())).isTrue();
+		assertThat(backOffPolicy instanceof ExponentialBackOffPolicy).isTrue();
 		ExponentialBackOffPolicy policy = (ExponentialBackOffPolicy) backOffPolicy;
 		assertThat(policy.getInitialInterval()).isEqualTo(100);
 		assertThat(policy.getMaxInterval()).isEqualTo(1000);
