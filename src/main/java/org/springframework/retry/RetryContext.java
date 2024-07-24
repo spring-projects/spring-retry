67996bee664be6019e520a21c4c0e591513efbfc
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2024-07-24 23:33:55 +0200

    Rephrase Javadoc for RetryContext.getLastThrowable()

--- src/main/java/org/springframework/retry/RetryContext.java
+++ src/main/java/org/springframework/retry/RetryContext.java
@@ -99,8 +99,8 @@ public interface RetryContext extends AttributeAccessor {
 	/**
 	 * Accessor for the exception object that caused the current retry.
 	 * @return the last exception that caused a retry, or possibly null. It will be null
-	 * if this is the first attempt, but also if the enclosing policy decides not to
-	 * provide it (e.g. because of concerns about memory usage).
+	 * if this is the first attempt and it finishes successfully, but also if the enclosing
+	 * policy decides not to provide it (e.g. because of concerns about memory usage).
 	 */
 	@Nullable
 	Throwable getLastThrowable();
