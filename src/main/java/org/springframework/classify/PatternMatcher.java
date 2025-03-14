2186e1155365679ea937c42cae0497404af9615f
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2025-03-14 21:19:02 +0700

    Remove unused import
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- src/main/java/org/springframework/classify/PatternMatcher.java
+++ src/main/java/org/springframework/classify/PatternMatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2022 the original author or authors.
+ * Copyright 2006-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
 package org.springframework.classify;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
